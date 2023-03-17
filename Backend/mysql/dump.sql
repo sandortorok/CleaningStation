cb17b48b4ff888e70172cfe3cbad282a6572f1b0
sandortorok <sandort99@gmail.com>
2023-03-17 13:08:57 +0100

    sql

---
+++ Backend/mysql/dump.sql
@@ -0,0 +1,158 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: cleaningStation
+-- ------------------------------------------------------
+-- Server version	8.0.32-0ubuntu0.22.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aInput`
+--
+
+DROP TABLE IF EXISTS `aInput`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aInput` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `value` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aInput`
+--
+
+LOCK TABLES `aInput` WRITE;
+/*!40000 ALTER TABLE `aInput` DISABLE KEYS */;
+INSERT INTO `aInput` VALUES (1,'wIn','Bejövő víznyomás [bar]',1.4),(2,'wOut','Kimenő víznyomás [bar]',5.5),(3,'wGoal','Cél víznyomás [bar]',5);
+/*!40000 ALTER TABLE `aInput` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `errorMessages`
+--
+
+DROP TABLE IF EXISTS `errorMessages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `errorMessages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `message` varchar(100) DEFAULT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `errorMessages`
+--
+
+LOCK TABLES `errorMessages` WRITE;
+/*!40000 ALTER TABLE `errorMessages` DISABLE KEYS */;
+INSERT INTO `errorMessages` VALUES (1,'Teszt üzenet','2023-02-28 09:40:05'),(2,'Teszt üzenet2','2023-02-28 09:40:10'),(3,'Teszt üzenet2','2023-02-28 09:40:37'),(4,'Teszt üzenet2','2023-02-28 09:40:37'),(5,'Teszt üzenet2','2023-02-28 09:40:37'),(6,'Teszt üzenet2','2023-02-28 09:40:37');
+/*!40000 ALTER TABLE `errorMessages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `frequencies`
+--
+
+DROP TABLE IF EXISTS `frequencies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `frequencies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `value` double NOT NULL,
+  `idx` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `frequencies`
+--
+
+LOCK TABLES `frequencies` WRITE;
+/*!40000 ALTER TABLE `frequencies` DISABLE KEYS */;
+INSERT INTO `frequencies` VALUES (1,'1-es Frekvenciaváltó',NULL,0,2),(2,'2-es Frekvenciaváltó',NULL,0,0),(3,'3-as Frekvenciaváltó',NULL,0,1);
+/*!40000 ALTER TABLE `frequencies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `input`
+--
+
+DROP TABLE IF EXISTS `input`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `input` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `is_on` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `input`
+--
+
+LOCK TABLES `input` WRITE;
+/*!40000 ALTER TABLE `input` DISABLE KEYS */;
+INSERT INTO `input` VALUES (1,'M1Error','Motor 1 Hiba (Motor 1 klixon + tiltó)',0),(2,'M2Error','Motor 2 Hiba (Motor 2 klixon + tiltó)',0),(3,'M3Error','Motor 3 Hiba (Motor 3 klixon + tiltó)',0),(4,'Water_on','Víz áramlás kapcsoló',0),(5,'Start','Program Start nyomógomb',1),(6,'Stop','Program Stop nyomógomb',0),(7,'F1Error','Frekvencia váltó 1 hiba',0),(8,'F2Error','Frekvencia váltó 2 hiba',0),(9,'F3Error','Frekvencia váltó 3 hiba',0),(10,'Auto','Szivattyúk automata üzem engedély (Auto/Kézi kapcsolók állása)',1);
+/*!40000 ALTER TABLE `input` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `output`
+--
+
+DROP TABLE IF EXISTS `output`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `output` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `is_on` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `output`
+--
+
+LOCK TABLES `output` WRITE;
+/*!40000 ALTER TABLE `output` DISABLE KEYS */;
+INSERT INTO `output` VALUES (1,'Running','Üzemel - a program fut',1),(2,'Errors','Gyűjtött hiba kimenet - minden hiba esetén ez a lámpa gyullad ki',0),(3,'F1Motor','Frekvenciaváltó 1 Motor',1),(4,'F2Motor','Frekvenciaváltó 2 Motor',1),(5,'F3Motor','Frekvenciaváltó 3 Motor',1);
+/*!40000 ALTER TABLE `output` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-17 13:08:29
